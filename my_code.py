4c9ad47253f4a6153df52fe5481852f2d33750ab
Munna Kumar <munnakumar6204835946@gmail.com>
2026-06-15 23:22:05 +0530

    Third Change

--- my_code.py
+++ my_code.py
@@ -1,6 +1,18 @@
 import pandas as pd
 
-df = pd.read_csv("students.csv")
+df = pd.read_csv("data\students.csv")
 
+name = input("Enter student name: ")
+marks = int(input("Enter marks: "))
+
+new_student = {
+    "name": name,
+    "marks": marks
+}
+
+df = pd.concat([df, pd.DataFrame([new_student])], ignore_index=True)
+
+df.to_csv("data/students.csv", index=False)
+
+print("Student added successfully!")
 print(df)
-print("Average Marks:", df["marks"].mean())
